5f9a9c51f0c76794177265cbfd3b50e42076322e
andouglasjr <andouglasjr@gmail.com>
2025-06-30 13:49:13 -0300

    coap protocol updated

--- coap/client_esp.py
+++ coap/client_esp.py
@@ -1,11 +1,11 @@
-from microcoapy.microcoapy import Coap
+from microcoapy import Coap
 import network
 import time
 
 # Conecta à rede Wi-Fi
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
-wlan.connect('your-ssid', 'your-password')
+wlan.connect('laica_iot_ap', '12345678')
 while not wlan.isconnected():
     pass
 
@@ -14,15 +14,19 @@ coap = Coap()
 coap.start()
 
 # Define o IP do servidor CoAP e a porta
-server_ip = '192.168.1.100'
+server_ip = '192.168.137.94'
 server_port = 5683
 
-# Envia um pacote CoAP POST para o servidor
-uri_path = 'sensor/data'
+def send_data(payload):
+    # Envia um pacote CoAP GET para o servidor
+    uri_path = 'sensor/data'
+    coap.post(server_ip, server_port, uri_path, bytearray(payload, 'utf-8'), None, 0)
+
 payload = 'temperature=24.5&humidity=60'
-coap.post(server_ip, server_port, uri_path, payload, None, Coap.CONTENT_FORMAT.COAP_TEXT_PLAIN)
+send_data(payload)
+
 
 # Aguarda um pouco antes de fechar
 time.sleep(2)
-
-coap.stop()
+def close_coap():
+    coap.stop()
