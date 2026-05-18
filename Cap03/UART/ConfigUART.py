a4b6043e5a0441836255212892e1227629b1b9f2
andouglasjr <andouglasjr@gmail.com>
2026-05-18 08:18:37 -0300

    Organização em pastas

---
+++ Cap03/UART/ConfigUART.py
@@ -0,0 +1,10 @@
+# Exemplo de configuração da porta UART usando a biblioteca machine do MicroPython.
+# Pode-se usar uma das duas configurações abaixo, dependendo dos requisitos do projeto.
+
+import machine
+
+# Configura a primeira porta UART com uma taxa de baud de 9600 bps
+uart = machine.UART(0, baudrate=9600)
+
+# Configura a primeira porta UART com uma taxa de baud de 9600 bps, 8 bits de dados, paridade par e 1 bit de parada.
+uart = machine.UART(0, baudrate=9600, bits=8, parity=1, stop=1)
---
+++ Outros/README.md
@@ -0,0 +1,2 @@
+# Resetar a placa
+Nesta pasta você encontra o arquivo UF2 que reseta o raspberry pi pico W.
